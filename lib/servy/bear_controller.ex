ab7a528b0ef292c43223c34bdb4789a993d29ab7
Matheus Monteiro <matheusmonteiro.home@gmail.com>
2024-05-22 23:35:01 -0300

    feat: adiciona paginas eex para mostrar uma lista de ursos ou um unico urso

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,25 +1,30 @@
 defmodule Servy.BearController do
-
   alias Servy.Wildthings
   alias Servy.Bear
 
-  def bear_item(bear) do
-    "<li>#{bear.name} = #{bear.type}</li>"
+  @templates_path Path.expand("../../templates", __DIR__)
+
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{conv | status: 200, body: content}
   end
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
 
-    %{conv | status: 200, body: "<ul>#{items}</ul>"}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Servy.Wildthings.get_bear(id)
-    %{conv | status: 200, body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type} = _params) do
